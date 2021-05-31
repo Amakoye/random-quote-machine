5caa1ef240bce44bd64ae69023fff4ecd0532323
Amakoye <charlesamakoye@gmail.com>
2021-05-31 13:09:07 +0300

    completed UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <div id="quote-box">
+        <div className="quote-text">
+          <i className="fa fa-quote-left"></i>
+          <span id="text">
+            Definiteness of purpose is the starting point of all achievement.
+          </span>
+        </div>
+        <div className="quote-author">
+          <p id="author">-Abraham Lincoln</p>
+        </div>
+        <div className="quote-buttons">
+          <a
+            href="twitter.com/intent/tweet"
+            className="button "
+            id="tweet-quote"
+            target="_blank"
+          >
+            <i className="fa fa-twitter"></i>
+          </a>
+          <button className="button" id="new-quote">
+            New quote
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
